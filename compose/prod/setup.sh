12fce8492e40dc1afa82415ed8b2dd690ddb3243
Vishal Menon <vishalmenon.92@gmail.com>
2022-04-13 23:46:27 +0530

    Updated nginx confer

--- compose/prod/setup.sh
+++ compose/prod/setup.sh
@@ -87,6 +87,7 @@ echo "[*] Generated new server signing keys."
 REPO="$(dirname $(dirname $(pwd)))"
 REPO_NGINX_CONF="$REPO/infra/nginx/prod/nginx-conf"
 rm -rf "$REPO_NGINX_CONF/default.conf"
+rm -rf "$REPO_NGINX_CONF/pre" "$REPO_NGINX_CONF/post"
 
 cp "$REPO_NGINX_CONF/pre_template" "$REPO_NGINX_CONF/pre" 
 perl -i -pe"s/___DOMAIN___/$MY_DOMAIN_NAME/g" "$REPO_NGINX_CONF/pre"
