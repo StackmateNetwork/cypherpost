fd8bfe3b0e8d3d89f9d5ed40d45450093d2704c2
Vishal Menon <vishalmenon.92@gmail.com>
2022-04-13 22:48:17 +0530

    Updated prod compose scripts

--- compose/delete_volumes.sh
+++ compose/delete_volumes.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-docker volume rm      compose_cp-configdb
-docker volume rm      compose_cp-datadb
-docker volume rm      compose_cp-server-code
-docker volume rm      compose_cp-server-keys
-docker volume rm      compose_cp-server-logs
-docker volume rm      compose_cp-web-root
+docker volume rm      dev_cp-configdb
+docker volume rm      dev_cp-datadb
+docker volume rm      dev_cp-server-code
+docker volume rm      dev_cp-server-keys
+docker volume rm      dev_cp-server-logs
+docker volume rm      dev_cp-web-root
+
+docker volume rm     prod_certbot-etc
+docker volume rm     prod_certbot-var
+docker volume rm     prod_cp-configdb
+docker volume rm     prod_cp-datadb
+docker volume rm     prod_cp-server-keys
+docker volume rm     prod_cp-server-logs
+docker volume rm     prod_dhparam
+docker volume rm     prod_web-root
--- compose/prod/start.sh
+++ compose/prod/start.sh
@@ -1,18 +1,11 @@
 #!/bin/bash -e
-
-# Create default.conf nginx using pre.conf
-REPO="$(dirname $(dirname $(pwd)))"
-NGINX_CONF="$REPO/infra/nginx/prod/nginx-conf"
-cat $NGINX_CONF/pre.conf
-cp $NGINX_CONF/pre.conf $NGINX_CONF/default.conf
-# Run docker-compose up to aquire SSL certificates
-docker-compose up -d
-# Run docker-compose down
-docker-compose down
-# Create default.conf nginx using post.conf
-cp $NGINX_CONF/post.conf $NGINX_CONF/default.conf
 # Change CERTBOT_RUNMODE to dry-run
 head -n -1 .env > temp.env ; mv temp.env .env
 echo "CERTBOT_RUNMODE=--dry-run" >> .env
+
+REPO="$(dirname $(dirname $(pwd)))"
+NGINX_CONF="$REPO/infra/nginx/prod/nginx-conf"
+cp $NGINX_CONF/post.conf $NGINX_CONF/default.conf
+
 # Re-run docker-compose up
 docker-compose up -d
